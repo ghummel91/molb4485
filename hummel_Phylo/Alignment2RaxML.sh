b6df4b336c443f2cea0892a68e7705eb975110d7
ghummel91 <falter3@live.com>
2019-11-19 14:42:34 -0700

    Finished bear trees

---
+++ hummel_Phylo/Alignment2RaxML.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J RAxML
+#SBATCH -n 1
+#SBATCH -t 60:00
+#SBATCH --mem=20g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ghummel@uwyo.edun
+
+#SBATCH --ntasks-per-node=8
+#SBATCH --account=inbre-train
+#SBATCH partition=inv-inbre
+
+echo "Starting raxmlHPC run at $(date)"
+
+module load swset gcc raxml
+
+raxmlHPC-PTHREADS-AVX -T 8 -f a -m GTRGAMMA -p 618 -N 50  -x 309 -s AllBears.aln -n AllBears -w /project/inbre-train/inbre011/molb4485/hummel_Phylo/BearTrees/Alignment2  -o GiantPanda
+
+echo "RAxML has finished at $(date)"
